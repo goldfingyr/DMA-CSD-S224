02bcad68da6446e0a61ddd7bedd2a3d82fd2f3a5
Karsten Jeppesen <kaje@ucn.dk>
2023-09-20 11:28:56 +0200

    test is SendTelegram-hc: Initital  commmit

---
+++ test/test/Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace test.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        private async void sendmessage(string message)
+        {
+
+        }
+
+        public void OnPost(string action="ERROR", string myText="ERROR")
+        {
+        }
+    }
+}
